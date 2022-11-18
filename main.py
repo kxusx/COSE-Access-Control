a7b0f731aab30385084e064ec77d523087ec76e9
kxusx <khush99199@gmail.com>
2022-11-18 18:12:00 +0530

    a

--- main.py
+++ main.py
@@ -1,9 +1,38 @@
-import os
+import pycose
+import pycose.keys
 
-import p
-fjwjf
+# Create a new key
+key = pycose.keys.CoseKey.from_dict({
+    'kty': 'EC',
+    'crv': 'P-256',
+    'x': 'x',
+    'y': 'y',
+    'd': 'd'
+})
 
-import pef
+# Create a new COSE_Sign1 object
+cose = pycose.CoseSign1(
+    payload=b'Hello World',
+    external_aad=b'External AAD',
+    key=key
+)
+
+# Sign the COSE_Sign1 object
+cose.sign()
+
+# Serialize the COSE_Sign1 object
+cose.serialize()
+
+# Deserialize the COSE_Sign1 object
+cose.deserialize()
+
+# Verify the COSE_Sign1 object
+cose.verify()
+
+# Get the payload
+cose.payload
+
+
 
 
 
